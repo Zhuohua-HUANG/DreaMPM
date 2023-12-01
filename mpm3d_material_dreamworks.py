f9507d94d3ebcea1f500150e6d75d00b35fa9b53
Zhuohua Huang <zhuohua.huang1@gmail.com>
2023-12-02 00:43:07 +0800

    update mpm3d_material_dreamworks.py

--- mpm3d_material_dreamworks.py
+++ mpm3d_material_dreamworks.py
@@ -5,8 +5,8 @@ ti.init(arch=ti.gpu)
 # Hyperparameters
 width, height = 1920, 1080
 
-G_number, max_timestep, max_hard = 64, 2, 2.5  # more particles, num: 65536
-# G_number, max_timestep, max_hard = 32, 4, 4  # less particles, num: 8192
+# G_number, max_timestep, max_hard = 64, 2, 2.5  # more particles, num: 65536
+G_number, max_timestep, max_hard = 32, 4, 4  # less particles, num: 8192
 
 config_path = './config.json'
 
